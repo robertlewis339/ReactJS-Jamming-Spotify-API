6d9a4908cdecee3987b4ed956402a25a6ca72b7b
Rob Lewis <robertlewis339@gmail.com>
2021-11-25 16:09:38 +0000

    Commiting app

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Playlist.css';
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
+  render() {
+    return <div className="Playlist">
+      <input 
+      defaultValue="New Playlist" 
+      onChange={this.handlenNameChange} 
+      />
+      <TrackList 
+      tracks={this.props.playListTracks}
+      onRemove={this.props.onRemove}
+      isRemoval={true}
+       />
+      <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+    </div>
+  }
+}
+
+export default Playlist;
+
+
